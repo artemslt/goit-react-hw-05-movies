ebce5f2770fbe359553601fcaf1ddd45ba8740b9
artemslt <101586284+artemslt@users.noreply.github.com>
2022-11-08 19:05:24 +0000

    Deploying to gh-pages from @ artemslt/goit-react-hw-05-movies@8f104cdf28c77b73e620f770fe9d21554bcf041d 🚀

---
+++ static/js/805.459f49fa.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[805],{9805:function(e,i,n){n.r(i);var t=n(184);i.default=function(){return(0,t.jsx)("main",{children:(0,t.jsx)("h2",{children:"PAGE NOT FOUND"})})}}}]);
+//# sourceMappingURL=805.459f49fa.chunk.js.map
